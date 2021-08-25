860f6b780c180efc9d98de428d2a771230306cdc
Rekha Yallatti <ryallatti97@gmail.com>
2021-08-25 16:20:53 +0530

    second commit from cmdline

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace GitFromCmd
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! hello honey");
                 });
             });
         }
